021f2d8323e1399ff5379cf4f9a1d0403e5aed2d
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2021-09-24 19:17:46 +0100

    added unit tests

--- src/test/java/dev/rexijie/oauth/oauth2server/model/dto/ClientDTOTest.java
+++ src/test/java/dev/rexijie/oauth/oauth2server/model/dto/ClientDTOTest.java
@@ -10,35 +10,11 @@ import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Set;
 
+import static dev.rexijie.oauth.oauth2server.mocks.ClientMocks.createTestClient;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClientDTOTest {
-    Client client = new Client(
-            null,
-            "Test client",
-            ClientTypes.PUBLIC.toString(),
-            ClientProfiles.WEB.toString(),
-            null,
-            null,
-            Set.of("read"),
-            Set.of("OAuthServer"),
-            Set.of("authorization_code", "implicit"),
-            Set.of("http://localhost:8081/oauth/code"),
-            Set.of("ROLE_USER", "ROLE_ADMIN"),
-            36,
-            3600,
-            Map.of(),
-            "http://localhost:8080/favicon.png",
-            "httpL//localhost:8080/",
-            null,
-            "http://localhost:8080/meta/redirects.json",
-            "",
-            null,
-            3600,
-            false,
-            TimeUtils.localDateTimeToEpochSecond(LocalDateTime.now()),
-            TimeUtils.localDateTimeToEpochSecond(LocalDateTime.now())
-    );
+    Client client = createTestClient();
 
     @Test
     void canMapToDto() {
