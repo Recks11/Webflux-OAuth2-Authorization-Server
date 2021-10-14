30d6e3f91d412ded559b711c9662e11a26847037
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2021-10-14 01:23:41 +0100

    updated utility classes

--- src/main/java/dev/rexijie/oauth/oauth2server/util/TimeUtils.java
+++ src/main/java/dev/rexijie/oauth/oauth2server/util/TimeUtils.java
@@ -1,9 +1,11 @@
 package dev.rexijie.oauth.oauth2server.util;
 
+import java.util.Date;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
 
 public class TimeUtils {
     public static final String TIME_ZONE = "GMT+1";
@@ -14,5 +16,16 @@ public class TimeUtils {
 
     public static long localDateTimeToEpochSecond(LocalDateTime localDateTime) {
         return localDateTime.toInstant(ZoneOffset.UTC).getEpochSecond();
+    }
+
+    public static Date secondsFromNow(int seconds) {
+        return fromNow(seconds, ChronoUnit.SECONDS);
+    }
+    public static Date minutesFromNow(int minutes) {
+        return fromNow(minutes, ChronoUnit.MINUTES);
+    }
+
+    private static Date fromNow(int time, ChronoUnit unit) {
+        return Date.from(Instant.now().plus(time, unit));
     }
 }
